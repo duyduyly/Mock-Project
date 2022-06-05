aeb368725f3efc6b2c41b8d3f9e9738e0f2cf0ec
duyduyly <59599590+duyduyly@users.noreply.github.com>
2022-06-05 23:25:19 +0700

    Delete mock-project/mock-project directory

--- mock-project/mock-project/src/main/java/com/example/demo/constants/SystemEmail.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.constants;
-
-public class SystemEmail {
-
-	public static final String EMAIL = "Duyduyly123vn@gmail.com";
-	public static final String PASSWORD = "012828337755";
-	
-}
