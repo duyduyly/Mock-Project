7c1e3960cdba3b85260d43dd3274c73b0550b35d
duyduyly123vn <Duyduyly123vn@gmail.com>
2022-06-05 23:21:17 +0700

    add

---
+++ mock-project/mock-project/src/main/java/com/example/demo/constants/SystemEmail.java
@@ -0,0 +1,8 @@
+package com.example.demo.constants;
+
+public class SystemEmail {
+
+	public static final String EMAIL = "Duyduyly123vn@gmail.com";
+	public static final String PASSWORD = "012828337755";
+	
+}
