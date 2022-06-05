d2322096a0f624af8e06a23d714e29afa47e3251
duyduyly <59599590+duyduyly@users.noreply.github.com>
2022-06-06 00:06:28 +0700

    Update SystemEmail.java

--- mock-project/src/main/java/com/example/demo/constants/SystemEmail.java
+++ mock-project/src/main/java/com/example/demo/constants/SystemEmail.java
@@ -3,6 +3,6 @@ package com.example.demo.constants;
 public class SystemEmail {
 
 	public static final String EMAIL = "Duyduyly123vn@gmail.com";
-	public static final String PASSWORD = "012828337755";
+	public static final String PASSWORD = "  ";
 	
 }
